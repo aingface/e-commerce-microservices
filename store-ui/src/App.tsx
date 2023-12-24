313b32a4326f8b0005b7c608b3f2c623367d5cfa
배대철 <qoeocjf5@gmail.com>
2023-12-24 14:48:05 +0900

    feat: Login 에서 Signin 으로 변경

--- store-ui/src/App.tsx
+++ store-ui/src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Cart from "./pages/Cart/Cart";
 import Layout from "./components/layout/Layout";
-import Login from "./pages/Login/Login";
+import SignIn from "./pages/SignIn/SignIn";
 import MyPage from "./pages/MyPage/MyPage";
 
 const App = (props: any) => {
@@ -13,7 +13,7 @@ const App = (props: any) => {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/MyPage" element={<MyPage />} />
       </Routes>
     </Layout>
---
+++ store-ui/src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+
+const SignIn = () => {
+  // login ui here (use material ui) and use navigate to go to home page
+
+  return <div></div>;
+};
+
+export default SignIn;
