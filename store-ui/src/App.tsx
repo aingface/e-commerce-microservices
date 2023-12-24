37955a30c99d7cc149d25428acada3fdfe00b742
배대철 <qoeocjf5@gmail.com>
2023-12-24 14:29:02 +0900

    feat: router 추가 - 로그인 페이지 - 마이페이지

--- store-ui/src/App.tsx
+++ store-ui/src/App.tsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Cart from "./pages/Cart/Cart";
 import Layout from "./components/layout/Layout";
+import Login from "./pages/Login/Login";
+import MyPage from "./pages/MyPage/MyPage";
 
 const App = (props: any) => {
   return (
@@ -11,6 +13,8 @@ const App = (props: any) => {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/MyPage" element={<MyPage />} />
       </Routes>
     </Layout>
   );
