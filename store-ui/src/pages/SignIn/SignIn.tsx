f30094f1e742e266ab8f6e093a7fa3e94e896815
배대철 <qoeocjf5@gmail.com>
2023-12-24 17:39:55 +0900

    update: sign-in 페이지 레이아웃 추가 - 로그인 실패 시 문구 출력되게 수정 - 회원가입 버튼 추가

--- store-ui/src/pages/SignIn/SignIn.tsx
+++ store-ui/src/pages/SignIn/SignIn.tsx
@@ -10,76 +10,117 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    // 로그인 로직을 여기에 작성합니다.
+    // 로그인이 실패하면 setLoginError를 호출하여 loginError 상태를 true로 설정합니다.
+    // 예: setLoginError(true);
+
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      email: email,
+      password: password,
     });
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    setEmailError(!emailRegex.test(event.target.value));
+  };
+
+  const handleSignOn = () => {
+    navigate("/sign-up");
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
-        <Paper style={{ backgroundColor: "#e0e0e0", padding: "20px" }}>
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              이메일 로그인
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            이메일 로그인
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="이메일 주소"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={handleEmailChange}
+              error={emailError}
+              helperText={emailError ? "이메일 형식이 아닙니다." : ""}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="비밀번호"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSignIn}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="이메일 주소"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="비밀번호"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                로그인
-              </Button>
-            </Box>
+              로그인
+            </Button>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                backgroundColor: "white",
+                borderColor: "primary.main",
+                color: "primary.main",
+                borderStyle: "solid",
+                borderWidth: "1px",
+                mb: 1,
+              }}
+              onClick={handleSignOn}
+            >
+              회원가입
+            </Button>
+
+            {loginError && (
+              <Typography color="error" fontSize={15}>
+                이메일 또는 비밀번호가 올바르지 않습니다. 다시 확인해주세요.
+              </Typography>
+            )}
           </Box>
-        </Paper>
+        </Box>
       </Container>
     </ThemeProvider>
   );
