071c8ada0a9d6afd88e89105f9382e736e0aa9b6
배대철 <qoeocjf5@gmail.com>
2023-12-25 16:24:51 +0900

    feat: 특정 사용자의 장바구니 업데이트 되는 api 추가

--- cart-cna-microservice/src/main/java/com/ecommerce/cart/model/Cart.java
+++ cart-cna-microservice/src/main/java/com/ecommerce/cart/model/Cart.java
@@ -6,12 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class Cart {
+    @NonNull
     private String customerId;
     private List<CartItem> items;
     private float total;
